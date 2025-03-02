3bd028142de20536cbcee4e0900be4c6d7ac7ae5
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-03-02 10:36:44 -0600

    enable optimizations

--- src/llvm_gen.rs
+++ src/llvm_gen.rs
@@ -247,12 +247,12 @@ impl<'a> LLVMGenerator<'a> {
 
         let fpm = PassManager::create(&self.module);
 
-        //fpm.add_instruction_combining_pass();
-        //fpm.add_reassociate_pass();
-        //fpm.add_gvn_pass();
-        //fpm.add_cfg_simplification_pass();
-        //fpm.add_basic_alias_analysis_pass();
-        //fpm.add_promote_memory_to_register_pass();
+        fpm.add_instruction_combining_pass();
+        fpm.add_reassociate_pass();
+        fpm.add_gvn_pass();
+        fpm.add_cfg_simplification_pass();
+        fpm.add_basic_alias_analysis_pass();
+        fpm.add_promote_memory_to_register_pass();
 
         fpm.initialize();
 
